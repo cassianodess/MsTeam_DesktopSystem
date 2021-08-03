6a367d160ea043f46943d4e361e56b9278029d89
Cassiano de Souza <cassianodess@hotmail.com>
2021-08-03 14:54:42 -0300

    update

--- msTeamDB.sql
+++ msTeamDB.sql
@@ -1,15 +1,15 @@
-create database msteam;
-use msteam;
+-- create database msteam;
+ -- use msteam;
 
 create table aluno(
-id int not null auto_increment primary key,
-nome varchar(50) not null,
-sobrenome varchar(50) not null,
-nascimento date not null,
-endereco varchar(50) not null,
+matricula int not null auto_increment primary key,
+nome varchar(20) not null,
+sobrenome varchar(20),
+nascimento date,
+endereco varchar(50),
 cpf varchar(14) unique,
-telefone varchar(14)
+telefone varchar(14),
+dia_vencimento int not null,
+data_vencimento date not null,
+situacao enum('Pago', 'Pendente')
 );
-
-select * from aluno;
-
