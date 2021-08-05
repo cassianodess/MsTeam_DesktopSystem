4f59784cee4cd03ce533768ba30e8e1fd9c4e236
Cassiano de Souza <cassianodess@hotmail.com>
2021-08-05 13:39:05 -0300

    upgrade Exceptions

--- msTeamDB.sql
+++ msTeamDB.sql
@@ -4,12 +4,14 @@
 create table aluno(
 matricula int not null auto_increment primary key,
 nome varchar(20) not null,
-sobrenome varchar(20),
-nascimento date,
+sobrenome varchar(40),
+nascimento date not null,
 endereco varchar(50),
-cpf varchar(14) unique,
+cpf varchar(14),
 telefone varchar(14),
 dia_vencimento int not null,
 data_vencimento date not null,
 situacao enum('Pago', 'Pendente')
 );
+
+select * from aluno;
